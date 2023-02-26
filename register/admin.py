ad090ec4f424314183ff036d649e9ea43c29379d
Henrique Lopes <riquellopes@gmail.com>
2023-02-26 15:30:22 -0300

    created new models

--- register/admin.py
+++ register/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
-from register.models import Card
+from register.models import (Card, Category, DollarToday, FollowUpCard, Installments)
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('name', )
+    list_display = ('name', )
 
-admin.site.register(Card)
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'dot', 'category', )
+
+@admin.register(DollarToday)
+class DollarTodayAdmin(admin.ModelAdmin):
+    
+    def has_add_permission(self, request, obj=None):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
+        return False
+
+@admin.register(FollowUpCard)
+class FollowUpCardAdmin(admin.ModelAdmin):
+    list_display = ('card', 'due_date', 'amount', 'notify', )
+
+@admin.register(Installments)
+class InstallmentsAdmin(admin.ModelAdmin):
+    list_display = ('card', 'amount', 'get_installments')
+
+    def get_installments(self, obj) -> str:
+        return "%d/%d" % (obj.current_installment, obj.total_installments)
+    get_installments.short_description = 'Installments'
--- register/models.py
+++ register/models.py
@@ -3,8 +3,40 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self) -> str:
+        return self.name
+
 class Card(models.Model):
     name = models.CharField(max_length=200)
-    dot = models.IntegerField(default=0)
+    dot = models.IntegerField(default=0) # @TODO update to float
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return self.name
+
+# https://dolarhoje.com/ptax/
+class DollarToday(models.Model):
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    create_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+
+class FollowUpCard(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    due_date = models.DateField()
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    notify = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True, editable=False)
+    update_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.card.name
+
+class Installments(models.Model):
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    current_installment = models.IntegerField(default=1)
+    total_installments = models.IntegerField(default=2)
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.card.name
